78c746d7d6f953ce56649d34f9baaed6dec11132
Atanu Majumder <atanumajumder2004@gmail.com>
2024-10-23 20:42:02 +0530

    refactor

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,10 +4,11 @@ const prismaClientSinglton = () => {
     return new PrismaClient()
 }
 
+type prismaClientSinglton = ReturnType<typeof prismaClientSinglton>;
+
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
 
 const prisma = globalForPrisma.prisma ?? prismaClientSinglton();
-
 
 export default prisma;
 
