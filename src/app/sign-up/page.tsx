e925258ff5b88acb900e6a242dca546164ff3903
Atanu Majumder <atanumajumder2004@gmail.com>
2024-10-26 11:17:14 +0530

    skeleton loading added

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -27,7 +27,20 @@ function Signup() {
 
   if (!isLoaded) {
     return (
-      <div>Loading.....</div>
+      <div className="flex items-center justify-center min-h-screen bg-background">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold text-center">Sign Up for Todo Master</CardTitle>
+        </CardHeader>
+        <CardContent>
+          {/* Display Skeletons */}
+          <Skeleton className="mb-4" />
+          <Skeleton className="mb-4" />
+          <Skeleton className="mb-4" />
+          <Skeleton className="mb-4" />
+        </CardContent>
+      </Card>
+    </div>
     )
   }
 
